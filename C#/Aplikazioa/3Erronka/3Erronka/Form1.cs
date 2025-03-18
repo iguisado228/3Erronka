0ad1880ca7faeb2c6a6119ce711eedd8211cd839
iguisado228 <ira_gui_lor@goierrieskola.org>
2025-03-18 11:44:29 +0100

    probatzeko komita

--- C#/Aplikazioa/3Erronka/3Erronka/Form1.cs
+++ C#/Aplikazioa/3Erronka/3Erronka/Form1.cs
@@ -16,5 +16,10 @@ namespace _3Erronka
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- WebOrria/src/views/ordutegiak.php
+++ WebOrria/src/views/ordutegiak.php
@@ -7,7 +7,64 @@
     <body class="main-grid">
         <?php require_once 'parts/header.php'; ?>
         <main>
-          
+            <div class="ordutegiaKontainer">
+                <h1>Asteko Ordutegiak</h1>
+                <div class="table-responsive">
+                    <table class="ordutegia-table">
+                        
+                            <tr>
+                                <th>Ordua</th>
+                                <th>Astelehena</th>
+                                <th>Asteartea</th>
+                                <th>Asteazkena</th>
+                                <th>Osteguna</th>
+                                <th>Ostirala</th>
+                            </tr>
+                        <tbody>
+                            <tr>
+                                <td>09:00</td>
+                                <td class="yoga">Yoga</td>
+                                <td class="pilates">Pilates</td>
+                                <td class="yoga">Yoga</td>
+                                <td class="pilates">Pilates</td>
+                                <td class="yoga">Yoga</td>
+                            </tr>
+                            <tr>
+                                <td>10:30</td>
+                                <td class="spinning">Spinning</td>
+                                <td class="zumba">Zumba</td>
+                                <td class="spinning">Spinning</td>
+                                <td class="zumba">Zumba</td>
+                                <td class="spinning">Spinning</td>
+                            </tr>
+                            <tr>
+                                <td>16:00</td>
+                                <td class="boxeo">Boxeoa</td>
+                                <td class="crossfit">Crossfit</td>
+                                <td class="boxeo">Boxeoa</td>
+                                <td class="crossfit">Crossfit</td>
+                                <td class="boxeo">Boxeoa</td>
+                            </tr>
+                            <tr>
+                                <td>17:30</td>
+                                <td class="zumba">Zumba</td>
+                                <td class="spinning">Spinning</td>
+                                <td class="zumba">Zumba</td>
+                                <td class="spinning">Spinning</td>
+                                <td class="zumba">Zumba</td>
+                            </tr>
+                            <tr>
+                                <td>19:00</td>
+                                <td class="pilates">Pilates</td>
+                                <td class="yoga">Yoga</td>
+                                <td class="pilates">Pilates</td>
+                                <td class="yoga">Yoga</td>
+                                <td class="pilates">Pilates</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </main>
         <?php require_once 'parts/footer.php'; ?>
     </body>
