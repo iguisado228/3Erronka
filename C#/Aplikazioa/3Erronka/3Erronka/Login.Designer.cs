3cad2f427bef77fa22d0ac96a5c9c56646bb73f5
otalavera06 <oie_tal_mun@goierrieskola.org>
2025-03-18 12:17:05 +0100

    Login pantallaren oinarria

--- C#/Aplikazioa/3Erronka/3Erronka/Login.Designer.cs
+++ C#/Aplikazioa/3Erronka/3Erronka/Login.Designer.cs
@@ -28,18 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.LBLIdentifikadorea = new System.Windows.Forms.Label();
+            this.BTNSaioaHasi = new System.Windows.Forms.Button();
+            this.TXTIdentifikadorea = new System.Windows.Forms.TextBox();
+            this.LBLPasahitza = new System.Windows.Forms.Label();
+            this.TXTPasahitza = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // LBLIdentifikadorea
+            // 
+            this.LBLIdentifikadorea.AutoSize = true;
+            this.LBLIdentifikadorea.Font = new System.Drawing.Font("Lucida Fax", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBLIdentifikadorea.Location = new System.Drawing.Point(243, 252);
+            this.LBLIdentifikadorea.Name = "LBLIdentifikadorea";
+            this.LBLIdentifikadorea.Size = new System.Drawing.Size(264, 36);
+            this.LBLIdentifikadorea.TabIndex = 0;
+            this.LBLIdentifikadorea.Text = "Identifikadorea";
+            // 
+            // BTNSaioaHasi
+            // 
+            this.BTNSaioaHasi.Font = new System.Drawing.Font("Lucida Fax", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNSaioaHasi.Location = new System.Drawing.Point(762, 527);
+            this.BTNSaioaHasi.Name = "BTNSaioaHasi";
+            this.BTNSaioaHasi.Size = new System.Drawing.Size(207, 49);
+            this.BTNSaioaHasi.TabIndex = 1;
+            this.BTNSaioaHasi.Text = "Saioa hasi";
+            this.BTNSaioaHasi.UseVisualStyleBackColor = true;
+            // 
+            // TXTIdentifikadorea
+            // 
+            this.TXTIdentifikadorea.Font = new System.Drawing.Font("Lucida Fax", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXTIdentifikadorea.Location = new System.Drawing.Point(536, 253);
+            this.TXTIdentifikadorea.Name = "TXTIdentifikadorea";
+            this.TXTIdentifikadorea.Size = new System.Drawing.Size(207, 35);
+            this.TXTIdentifikadorea.TabIndex = 2;
+            // 
+            // LBLPasahitza
+            // 
+            this.LBLPasahitza.AutoSize = true;
+            this.LBLPasahitza.Font = new System.Drawing.Font("Lucida Fax", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBLPasahitza.Location = new System.Drawing.Point(339, 314);
+            this.LBLPasahitza.Name = "LBLPasahitza";
+            this.LBLPasahitza.Size = new System.Drawing.Size(168, 36);
+            this.LBLPasahitza.TabIndex = 3;
+            this.LBLPasahitza.Text = "Pasahitza";
+            // 
+            // TXTPasahitza
+            // 
+            this.TXTPasahitza.Font = new System.Drawing.Font("Lucida Fax", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXTPasahitza.Location = new System.Drawing.Point(536, 315);
+            this.TXTPasahitza.Name = "TXTPasahitza";
+            this.TXTPasahitza.Size = new System.Drawing.Size(207, 35);
+            this.TXTPasahitza.TabIndex = 4;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1000, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::_3Erronka.Properties.Resources.BuruntzaldeaLogoa;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(490, 150);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.TXTPasahitza);
+            this.Controls.Add(this.LBLPasahitza);
+            this.Controls.Add(this.TXTIdentifikadorea);
+            this.Controls.Add(this.BTNSaioaHasi);
+            this.Controls.Add(this.LBLIdentifikadorea);
+            this.Controls.Add(this.menuStrip1);
             this.Name = "Login";
-            this.Size = new System.Drawing.Size(873, 425);
+            this.Size = new System.Drawing.Size(1000, 600);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label LBLIdentifikadorea;
+        private System.Windows.Forms.Button BTNSaioaHasi;
+        private System.Windows.Forms.TextBox TXTIdentifikadorea;
+        private System.Windows.Forms.Label LBLPasahitza;
+        private System.Windows.Forms.TextBox TXTPasahitza;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- C#/Aplikazioa/3Erronka/3Erronka/Login.cs
+++ C#/Aplikazioa/3Erronka/3Erronka/Login.cs
@@ -16,5 +16,10 @@ namespace _3Erronka
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
