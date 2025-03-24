50659c48695240c396156d31072763f917553993
iguisado228 <ira_gui_lor@goierrieskola.org>
2025-03-24 13:40:23 +0100

    jardueren atala sortuta

--- WebOrria/src/views/jarduerak.php
+++ WebOrria/src/views/jarduerak.php
@@ -7,7 +7,70 @@
     <body class="main-grid">
         <?php require_once 'parts/header.php'; ?>
         <main>
-           
+            <div class="jarduerakKontainer2">
+                <h1>Gure Jarduerak</h1>
+                <div class="jarduerakGrid2">
+                    <div class="jarduerak2">
+                        <img src="../../public/irudiak/spinning.jpg" alt="Spinning">
+                        <h3>Spinning</h3>
+                        <p>Intentsitate handiko kardio entrenamendua, musika energetikoarekin.</p>
+                        <div class="jardueraInfo2">
+                            <span>Iraupena: 45 min</span>
+                            <span>Zailtasuna: Ertaina</span>
+                        </div>
+                    </div>
+
+                    <div class="jarduerak2">
+                        <img src="../../public/irudiak/yoga.jpg" alt="Yoga">
+                        <h3>Yoga</h3>
+                        <p>Gorputza eta burua lasaitzeko jarduera, malgutasuna hobetzeko.</p>
+                        <div class="jardueraInfo2">
+                            <span>Iraupena: 60 min</span>
+                            <span>Zailtasuna: Erraza</span>
+                        </div>
+                    </div>
+
+                    <div class="jarduerak2">
+                        <img src="../../public/irudiak/pilates.jpg" alt="Pilates">
+                        <h3>Pilates</h3>
+                        <p>Gorputzaren indarra eta malgutasuna hobetzeko ariketa sistema.</p>
+                        <div class="jardueraInfo2">
+                            <span>Iraupena: 50 min</span>
+                            <span>Zailtasuna: Ertaina</span>
+                        </div>
+                    </div>
+
+                    <div class="jarduerak2">
+                        <img src="../../public/irudiak/zumba.jpg" alt="Zumba">
+                        <h3>Zumba</h3>
+                        <p>Dantza eta ariketa fisikoa konbinatzen dituen jarduera dibertigarria.</p>
+                        <div class="jardueraInfo2">
+                            <span>Iraupena: 45 min</span>
+                            <span>Zailtasuna: Ertaina</span>
+                        </div>
+                    </div>
+
+                    <div class="jarduerak2">
+                        <img src="../../public/irudiak/boxeo.jpg" alt="Boxeoa">
+                        <h3>Boxeoa</h3>
+                        <p>Defentsa pertsonala eta fitness-a konbinatzen dituen entrenamendua.</p>
+                        <div class="jardueraInfo2">
+                            <span>Iraupena: 60 min</span>
+                            <span>Zailtasuna: Zaila</span>
+                        </div>
+                    </div>
+
+                    <div class="jarduerak2">
+                        <img src="../../public/irudiak/crossfit.jpg" alt="Crossfit">
+                        <h3>Crossfit</h3>
+                        <p>Intentsitate handiko entrenamendu funtzionala.</p>
+                        <div class="jardueraInfo2">
+                            <span>Iraupena: 50 min</span>
+                            <span>Zailtasuna: Zaila</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </main>
         <?php require_once 'parts/footer.php'; ?>
     </body>
