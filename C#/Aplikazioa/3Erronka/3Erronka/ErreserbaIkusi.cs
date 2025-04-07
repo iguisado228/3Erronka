26e5061a00267017ab7ccf03849583bd92588b6e
iguisado228 <ira_gui_lor@goierrieskola.org>
2025-04-07 10:13:31 +0200

    c# diagramak eguneratuta

--- C#/Aplikazioa/3Erronka/3Erronka/ErreserbaIkusi.cs
+++ C#/Aplikazioa/3Erronka/3Erronka/ErreserbaIkusi.cs
@@ -23,8 +23,21 @@ namespace _3Erronka
             this.loggedInBazkidea = bazkidea ?? new Bazkidea(999);
         }
 
+
         private void ErreserbaIkusi_Load(object sender, EventArgs e)
         {
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.BackgroundColor = Color.FromArgb(240, 240, 240);
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.Gray;
+            dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.LightGray;
+
+
             Konexioa.Konexioa K = new Konexioa.Konexioa();
             K.konektatu();
 
@@ -32,14 +45,23 @@ namespace _3Erronka
             {
                 DataTable table = new DataTable();
 
-                MySqlCommand command = new MySqlCommand("SELECT e.idErreserba AS 'Erreserbaren IDa', k.izena AS 'Kluba', b.izena AS 'Bazkidea', er.izena AS 'Eremua', e.erreserbaEguna AS 'Eguna', e.hasieraOrdua AS 'Hasiera Ordua', e.amaieraOrdua AS 'Amaiera ordua' FROM erreserba e JOIN eremua er ON e.idEremua = er.idEremua LEFT JOIN kluba k ON e.idKluba = k.idKluba LEFT JOIN bazkidea b ON e.idBazkidea = b.idBazkidea WHERE 1=1", K.conn);
-
+                MySqlCommand command = new MySqlCommand("SELECT e.idErreserba AS 'Erreserbaren IDa', k.izena AS 'Kluba', " +
+                    "b.izena AS 'Bazkidea', er.izena AS 'Eremua', e.erreserbaEguna AS 'Eguna', " +
+                    "e.ordua AS 'OrduaZenbakia', CONCAT (7 + e.ordua, ':00 - ', 7 + e.ordua + 1, ' :00') AS 'Ordua' FROM erreserba e" +
+                    " JOIN eremua er ON e.idEremua = er.idEremua LEFT JOIN kluba k ON e.idKluba = k.idKluba" +
+                    " LEFT JOIN bazkidea b ON e.idBazkidea = b.idBazkidea WHERE 1=1", K.conn);
 
                 MySqlDataAdapter adapter = new MySqlDataAdapter(command);
 
                 adapter.Fill(table);
 
+
                 dataGridView1.DataSource = table;
+                dataGridView1.Columns.Cast<DataGridViewColumn>().ToList().ForEach(col =>
+                {
+                    if (col.Name == "OrduaZenbakia")
+                        col.Visible = false;
+                });
 
             }catch (Exception ex)
             {
